61e4e3577fc8283da78400ac573524341e9302a9
Vincent Rémond <Vincent Rmond>
2016-10-16 05:23:04 +0200

    Initial commit (get sources from FTP)

---
+++ functions.php
@@ -0,0 +1,166 @@
+<?php
+
+function try_get($search, $key, &$result, $default = null)
+{
+	if(array_key_exists($key, $search))
+	{
+		$result = $search[$key];
+		return TRUE;
+	}
+	else
+	{
+		$result = $default;
+		return FALSE;
+	}
+}
+
+function set_infinite_cookie($name, $value)
+{
+	setcookie($name, $value, time() + (20 * 365 * 24 * 60 * 60));
+}
+
+function getStyles($fontname, $fontsize, $topColor, $botColor, $forceBottom, &$styles, &$keys)
+{
+
+	$specifics = array
+		( 'Default' => array('PrimaryColour' => getColour('#FFFFFF') , "SecondaryColour" => getColour('#FFFFFF', null, 1.2), 'Alignment' => '2')
+		, 'Top'     => array('PrimaryColour' => getColour($topColor) , "SecondaryColour" => getColour($topColor, null, 1.2), 'Alignment' => ($forceBottom == '1' ? '2' : '8'))
+		, 'Mid'     => array('PrimaryColour' => getColour('#FFFF00') , "SecondaryColour" => getColour('#FFFFFF', null, 1.2), 'Alignment' => '5')
+		, 'Bot'     => array('PrimaryColour' => getColour($botColor) , "SecondaryColour" => getColour($botColor, null, 1.2), 'Alignment' => '2')
+		);
+	
+	$defaults = array(
+		'Fontname' => $fontname,
+		'Fontsize' => $fontsize,
+		'PrimaryColour' => getColour('#FFFFFF'),
+		'SecondaryColour' =>  getColour('#FFFFFF'),
+		'OutlineColour' =>  getColour('#000000'),
+		'BackColour' => getColour('#000000'),
+		'Bold' => '-1',
+		'Italic' => '0',
+		'Underline' => '0',
+		'StrikeOut' => '0',
+		'ScaleX' => '100',
+		'ScaleY' => '100',
+		'Spacing' => '0',
+		'Angle' => '0',
+		'BorderStyle' => '1',
+		'Outline' => '3',
+		'Shadow' => '0',
+		'Alignment' => '2',
+		'MarginL' => '10',
+		'MarginR' => '10',
+		'MarginV' => '10',
+		'Encoding' => '0',
+	);
+	
+	$keys = array_keys($defaults);
+	
+	$styles = array();
+	foreach($specifics as $key => $values)
+	{
+		$tmp = array();
+		foreach($defaults as $dk => $dv)
+		{
+			$tmp[$dk] = array_key_exists($dk, $values) ? $values[$dk] : $dv;
+		}
+		$styles[$key] = $tmp;
+	}
+}
+
+function getColour($color, $alpha = null, $change = null)
+{
+	if($alpha === null) $alpha = 0;
+	if($change === null) $change = 1.0;
+	if(!preg_match('/^#?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i', $color, $parts))
+		return "&H00FFFFFF";
+	$out = '&H';
+	$values = array($alpha);
+	for($i = 3; $i >= 1; $i--)
+	{
+		$value = hexdec($parts[$i]);
+		$value = intval(min(255, round($value * $change))); // 80/100 = 80%, i.e. 20% darker
+		$values[] = $value;
+	}
+	for($i = 0; $i < count($values); $i++)
+	{
+		$out .= str_pad(dechex($values[$i]), 2, '0', STR_PAD_LEFT);
+	}
+	return strtoupper($out);
+}
+
+function sendHTML($error = null)
+{
+    include('html.php');
+	die();
+}
+
+function cleanSRT(&$text)
+{
+	$text = strip_tags($text);
+
+	$text .= "\n";
+
+	$patterns = array();
+	$replacements = array();
+	// Windows to Unix
+	$patterns[]     = '/\r\n/';
+	$replacements[] = "\n";
+	// Too much spaces
+	$patterns[]     = '/[ \t]+\n/';
+	$replacements[] = "\n";
+	// Too much spaces
+	$patterns[]     = '/\n\n\n+/';
+	$replacements[] = "\n\n";
+	// Tags
+	$patterns[]     = '/\{\\\\pos\(\d+,\d+\)\}/';
+	$replacements[] = "";
+	
+	$text = preg_replace($patterns, $replacements, $text);
+	
+	return null;
+}
+
+function getTimeStamp($str) {
+	if($str[0] == '0') {
+		return substr($str, 1);
+	}
+	return $str;
+}
+
+function parseAndAddSRT($text,&$tree,$type)
+{
+	$eregEvent = '/\d+\n(\d\d:\d\d:\d\d),(\d\d)\d --> (\d\d:\d\d:\d\d),(\d\d)\d\n(.+?(\n.+?)*)\n\n/';
+	preg_match_all($eregEvent, $text, $matches);
+
+	if(count($matches[0]) === 0)
+		sendHTML('No subtitles found in SRT file for '.strtolower($type).'.');
+	
+	for ( $i = 0 ; $i < count($matches[0]) ; $i++ )
+	{
+		$tree[] = array(
+			'start' => $matches[1][$i].'.'.$matches[2][$i],
+			'end'   => $matches[3][$i].'.'.$matches[4][$i],
+			'type'  => $type,
+			'text'  =>  str_replace("\n", '\N', $matches[5][$i])
+		);
+	}
+	
+	return null;
+}
+
+function compare($a, $b)
+{
+	if($a['start'] < $b['start'])
+		return -1;
+	if($a['start'] > $b['start'])
+		return 1;
+	if($a['type'] < $b['type'])
+		return -1;
+	if($a['type'] > $b['type'])
+		return 1;
+	else
+		return 0;
+}
+
+?>
---
+++ html.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>2srt2ass</title>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="jscolor/jscolor.js"></script>
+		<link href="//fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css"></link>
+
+<script type="text/javascript">
+
+$(function() {
+	$('option').each(function(index, Element) {
+		$Element = $(Element);
+		$Element.text($Element.attr('value'));
+	});
+	
+	$("select[name='fontsize']").val('<?php try_get($_COOKIE, 'fontsize', $fontsize, '16'); echo $fontsize; ?>');
+	$("select[name='fontname']").val('<?php try_get($_COOKIE, 'fontname', $fontname, 'Arial'); echo $fontname; ?>');
+	$("input[name='topColor']").val('<?php try_get($_COOKIE, 'topColor', $topColor, '#FFFFF9'); echo $topColor; ?>');
+	$("input[name='botColor']").val('<?php try_get($_COOKIE, 'botColor', $botColor, '#F9FFF9'); echo $botColor; ?>');
+	$("input[name='forceBottom']").prop('checked', <?php try_get($_COOKIE, 'forceBottom', $forceBottom, '0'); echo ($forceBottom == '1' ? 'true' : 'false'); ?>);
+});
+
+</script>
+<style type="text/css">
+html { width: 100%; height: 100%; background: url("background.svg") no-repeat center center fixed; background-size: cover; }
+body { font-family: 'Noto Sans', sans-serif; margin: 0px; padding: 0px; color: #595959; }
+.page { width: 800px; margin: 25px auto; background-color: rgb(220, 220, 220); background-color: rgba(220, 220, 220, 0.75); }
+h1 { background-color: #595959; color: #999999; padding: 3px 15px; }
+h2 { background-color: #909090; color: #CCCCCC; padding: 3px 15px; }
+h3 { padding: 3px 15px; }
+p { padding: 0; margin: 20px 30px; }
+.error { margin: 20px; border: 1px solid #909090; }
+.error h2 { margin: 0; }
+.error p { font-weight: bold; margin: 10px 15px; color: #451C1C; }
+
+.main-form { padding: 0 0 0 30px; }
+.main-form form { }
+.main-form form .form-row { clear: both; margin: 0 0 7px 0; }
+.main-form form .form-row .form-label { width: 135px; float: left; }
+.main-form form .form-row .form-input { }
+.main-form form .form-row .form-input input { border: 1px solid #595959; }
+.main-form form .form-row .form-input input[type=file] { width: 600px; }
+
+</style>
+	</head>
+	<body>
+		<div class="page">
+			<h1 class="main-title">2srt2ass</h1>
+<?php if ($error != null) : ?>
+			<div class="error">
+				<h2>Error</h2>
+				<p><?= $error ?></p>
+			</div>
+<?php endif; ?>
+			<div class="main-form">
+				<form action="." method="post" enctype="multipart/form-data" name="form">
+					<input type="hidden" name="send" value="yes">
+					
+					<div class="form-row">
+						<span class="form-label"><label for="top">Top subtitle</label> :</span>
+						<span class="form-input"><input type="file" id="top" name="top" /></span>
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="bot">Bottom subtitle</label> :</span>
+						<span class="form-input"><input type="file" id="bot" name="bot" /></span>
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="fontsize">Font size</label> :</span>
+						<span class="form-input">
+							<select id="fontsize" name="fontsize">
+								<option value="12"></option>
+								<option value="14"></option>
+								<option value="16"></option>
+								<option value="18"></option>
+								<option value="20"></option>
+								<option value="22"></option>
+								<option value="24"></option>
+								<option value="26"></option>
+								<option value="28"></option>
+								<option value="80"></option>
+							</select>
+						</span>
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="fontname">Font name</label> :</span>
+						<span class="form-input">
+							<select id="fontname" name="fontname">
+								<option value="Arial"></option>
+								<option value="Comic Sans MS"></option>
+								<option value="Verdana"></option>
+							</select>
+						</span>
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="topColor">Top color</label> :</span>
+						<span class="form-input"><input type="text" id="topColor" name="topColor" class="color {hash:true}" />
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="botColor">Bottom color</label> :</span>
+						<span class="form-input"><input type="text" id="botColor" name="botColor" class="color {hash:true}" />
+					</div>
+					
+					<div class="form-row">
+						<span class="form-label"><label for="forceBottom">Force bottom</label> :</span>
+						<span class="form-input"><input type="checkbox" id="forceBottom" name="forceBottom" value="1" /><label for="forceBottom"> [EXPERIMENTAL]</label></span>
+				    </div>
+										
+					<input type="submit" />
+					
+				</form>
+			</div>
+			<div class="about">
+				<h2>About</h2>
+				<h3>How to use</h3>
+				<p>Upload 2 srt files, the script will send you a SSA/ASS subtitle files. This file will have on top the fisrt file's subtitles and on bottom those of the second file.</p>
+				<p>If you have any problem, make sure both your files are <a href="http://en.wikipedia.org/wiki/SubRip#SubRip_.srt_file_example">formatted in SubRip (srt)</a> and are using the same encoding (like UTF-8).</p>
+				<h3>Author</h3>
+				<p>2srt2ass developped by Vincent Rémond.</p>
+				<p>Feedback or bugs at <a href="mailto:vincent.remond@gmail.com?subject=Feedback%202SRT2ASS">vincent.remond@gmail.com</a>.</p>
+				<p>Program under GNU/GPL. <a href="sources/">Sources here</a></p>
+				<h3>Changelog</h3>
+				<ul>
+					<li><b>0.2.0</b> &ndash;  2016-09-27<br />- Added force subtitles to bottom</li>
+					<li><b>0.1.1</b> &ndash;  2015-06-20<br />- Remove leading 0 in timestamps</li>
+					<li><b>0.1.0</b> &ndash;  2013-04-07<br />- Improved UI<br />- Customizable font output</li>
+					<li><b>0.0.2</b><br />- Fixed : Only Windows' style line returns for output file<br />- Added : Generated file's name based on bottom file<br />- Fixed : only display GIF files when downloading on Firefox</li>
+					<li><b>0.0.1</b><br />- Fixed : debug remains<br />- Added : removal of HTML and position tags</li>
+					<li><b>0.0.0</b><br />- First release</li>
+				</ul>
+			</div>
+		</div>
+<?php include($_SERVER["DOCUMENT_ROOT"] . '/include/google-analytics.php'); ?>
+	</body>
+</html>
